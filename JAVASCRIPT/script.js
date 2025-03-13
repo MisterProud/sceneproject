098b90f2c3197f0de685472ae1895beaee0ebd14
MisterProud <proudmarkets@protonmail.com>
2025-03-13 18:52:51 -0400

    committ

--- JAVASCRIPT/script.js
+++ JAVASCRIPT/script.js
@@ -3,21 +3,21 @@ const ctx = canvas.getContext('2d');
 
 
 const backgroundImg = new Image();
-backgroundImg.src = '/Users/proudmarkets/sites/sceneproject.github.io/Images/background.jpg';
+backgroundImg.src = 'Images/background.jpg';
 
 backgroundImg.onload = function() {
   ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
 
 
   const foregroundImg1 = new Image();
-  foregroundImg1.src = '/Users/proudmarkets/sites/sceneproject.github.io/Images/foreground1.png';
+  foregroundImg1.src = 'Images/foreground1.png';
 
   foregroundImg1.onload = function() {
     ctx.drawImage(foregroundImg1, 100, 200, 200, 150);
   };
 
   const foregroundImg2 = new Image();
-  foregroundImg2.src = '/Users/proudmarkets/sites/sceneproject.github.io/Images/foreground2.png';
+  foregroundImg2.src = 'Images/foreground2.png';
 
   foregroundImg2.onload = function() {
     ctx.drawImage(foregroundImg2, 400, 300, 250, 200);
