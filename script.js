f33ef743eee6118698cd0be9edb3df6e5947de86
MisterProud <proudmarkets@protonmail.com>
2025-03-13 19:16:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,31 +1,37 @@
 const canvas = document.getElementById('myCanvas');
 const ctx = canvas.getContext('2d');
 
-
 const backgroundImg = new Image();
 backgroundImg.src = 'images/background.jpg';
 
 backgroundImg.onload = function() {
+  console.log('Background image loaded'); 
   ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
 
-
+  
   const foregroundImg1 = new Image();
   foregroundImg1.src = 'images/foreground1.png';
 
   foregroundImg1.onload = function() {
-    ctx.drawImage(foregroundImg1, 10, 20, 20, 15);
+    console.log('Foreground image 1 loaded'); 
+    ctx.drawImage(foregroundImg1, 100, 200, 200, 150);
   };
 
   const foregroundImg2 = new Image();
   foregroundImg2.src = 'images/foreground2.png';
 
   foregroundImg2.onload = function() {
-    ctx.drawImage(foregroundImg2, 40, 30, 25, 20);
+    console.log('Foreground image 2 loaded'); 
+    ctx.drawImage(foregroundImg2, 400, 300, 250, 200);
   };
 
   
   ctx.font = '40px Arial';
   ctx.fillStyle = 'white';
   ctx.fillText('Jason Proud', 50, 50);
-  ctx.fillText('The X Zone', 50, 100);
+  ctx.fillText('The Infinity Zone', 50, 100);
+};
+
+backgroundImg.onerror = function() {
+  console.error('Failed to load background image'); 
 };
