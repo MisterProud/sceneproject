4449602e2b0e64717adfbc10dd2e90e174e76235
MisterProud <proudmarkets@protonmail.com>
2025-03-13 18:59:05 -0400

    new

--- script.js
+++ script.js
@@ -3,21 +3,21 @@ const ctx = canvas.getContext('2d');
 
 
 const backgroundImg = new Image();
-backgroundImg.src = 'Images/background.jpg';
+backgroundImg.src = 'images/background.jpg';
 
 backgroundImg.onload = function() {
   ctx.drawImage(backgroundImg, 0, 0, canvas.width, canvas.height);
 
 
   const foregroundImg1 = new Image();
-  foregroundImg1.src = 'Images/foreground1.png';
+  foregroundImg1.src = 'images/foreground1.png';
 
   foregroundImg1.onload = function() {
     ctx.drawImage(foregroundImg1, 100, 200, 200, 150);
   };
 
   const foregroundImg2 = new Image();
-  foregroundImg2.src = 'Images/foreground2.png';
+  foregroundImg2.src = 'images/foreground2.png';
 
   foregroundImg2.onload = function() {
     ctx.drawImage(foregroundImg2, 400, 300, 250, 200);
